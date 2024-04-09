3fc5538e289dad207b7ddea43eaca42687899b95
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2024-04-09 13:01:49 -0600

    TIPADO, TIPOS INFERIDOS, FUNCIONES ANONIMAS AUTOEJECUTABLES

---
+++ src/03-typing.ts
@@ -0,0 +1,14 @@
+(() => {
+  let myProductName = 'Product 1'
+  let myProductPrice = 123
+
+  myProductName = 'change'
+  myProductName.toLowerCase()
+
+  myProductPrice.toFixed()
+
+  const myProductStock = 1000
+  const myProductName2 = 'Product 2'
+})() // funcion anonima autoejecutable
+
+//usar motor de inferencia, recomendado
